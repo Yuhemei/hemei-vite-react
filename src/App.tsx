5b6686d4f9ce20cabc26945c21ec01e55704313c
hemei <yuhemei8088@163.com>
2023-02-17 00:57:37 +0800

    初始化项目

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ViteReactDemo from './screens/ViteReactDemo/index.js'
+import './App.css'
+
+function App() {
+    return (
+        <div id="root">
+            {/* <ViteReactDemo></ViteReactDemo> */}
+        </div>
+    )
+}
+
+export default App
