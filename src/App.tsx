5543341e0b52d2d37804ba24abf5d4b13e1a247e
hemei <yuhemei8088@163.com>
2023-02-17 01:38:26 +0800

    feat(page):添加空格游戏

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import ViteReactDemo from './screens/ViteReactDemo'
-
+import { Game as SquireGame } from './screens/SquireGame'
 function App() {
     return (
         <div>
             <ViteReactDemo />
+            <SquireGame></SquireGame>
         </div>
     )
 }
