c3cf3ddcb459749e0f1693b7d055f293368a99ea
hemei <yuhemei8088@163.com>
2023-02-22 23:09:08 +0800

    feat(utils):完善utils注释

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,20 +1,21 @@
 /*
- * @Author: Hemei
+ * @Author: Hemei 
  * @Date: 2023-02-20 19:50:34
  * @FilePath: /vite-react-project/src/utils/index.js
  * @Description: 封装的工具函数
  */
 
-const debounceObj = {}
 /**
  * @description: 防抖函数
  * @param {uuid} funcName
- * @param {function} func
+ * @param {function} func //尽量使用箭头函数
  * @param {time} wait //秒为单位
  * @return {function}
  */
-export function debounce(funcName, func, wait = 0) {
+const debounceObj = {} //防抖宿主对象
+export function debounce(funcName, func, wait = 0.5) {
     return function () {
+        console.log("arguments", arguments);
         debounceObj[funcName] && clearTimeout(debounceObj[funcName])
         debounceObj[funcName] = setTimeout(() => {
             func()
