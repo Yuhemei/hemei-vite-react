33fa9947042c1da0b8e1deb49e3287af9623a481
yuhemei <yuhemei8088@163.com>
2023-02-28 11:12:37 +0800

    feat(file):修改路由文件引用方式

--- src/main.tsx
+++ src/main.tsx
@@ -1,49 +1,20 @@
+/*
+ * @Author: Hemei yuhemei8088@163.com
+ * @Date: 2023-02-24 14:56:22
+ * @FilePath: /hemei-vite-react/src/main.tsx
+ * @Description: 项目主文件
+ */
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+
 import {
     createBrowserRouter,
     RouterProvider,
 } from "react-router-dom";
-import "./index.css";
 
-import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
-import ErrorPage from "./error-page";
-import Contact, { loader as contactLoader } from "./routes/contact";
-import EditContact, { action as editAction } from "./routes/edit";
-import { action as destroyAction } from "./routes/destroy";
-import Index from "./routes/index";
+import router from '@/router.jsx'
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root></Root>,
-        errorElement: <ErrorPage />,
-        loader: rootLoader, //类似于Vue的mounted或者$route.query/params
-        action: rootAction,
-        children: [
-            { index: true, element: <Index /> },
-            // contact item
-            {
-                path: "contacts/:contactId",
-                loader: contactLoader,
-                element: <Contact />,
-            },
-            // 编辑
-            {
-                path: "contacts/:contactId/edit",
-                loader: contactLoader,
-                action: editAction,
-                element: <EditContact />,
-            },
-            // 删除
-            {
-                path: "contacts/:contactId/destroy",
-                action: destroyAction,
-                // errorElement: <div>Oops! There was an error.</div>,
-            },
-        ],
-    },
-]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
---
+++ src/router.jsx
@@ -0,0 +1,51 @@
+/*
+ * @Author: Hemei yuhemei8088@163.com
+ * @Date: 2023-02-28 09:39:38
+ * @FilePath: /hemei-vite-react/src/router.jsx
+ * @Description: 路由组件
+ */
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact, { loader as contactLoader } from "./routes/contact";
+import EditContact, { action as editAction } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage />,
+    loader: rootLoader, //类似于Vue的mounted或者$route.query/params
+    action: rootAction,
+    children: [
+      { index: true, element: <Index /> },
+      // contact item
+      {
+        path: "contacts/:contactId",
+        loader: contactLoader,
+        element: <Contact />,
+      },
+      // 编辑
+      {
+        path: "contacts/:contactId/edit",
+        loader: contactLoader,
+        action: editAction,
+        element: <EditContact />,
+      },
+      // 删除
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        // errorElement: <div>Oops! There was an error.</div>,
+      },
+    ],
+  },
+]);
+
+export default router
