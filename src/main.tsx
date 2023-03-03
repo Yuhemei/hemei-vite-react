edc45b89611a8593bf506cbbeadc042f6e776250
yuhemei <yuhemei8088@163.com>
2023-03-03 09:10:07 +0800

    feat(yarn):redux刚刚开始

--- src/main.tsx
+++ src/main.tsx
@@ -7,17 +7,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
+// 路由
 import {
-    createBrowserRouter,
     RouterProvider,
 } from "react-router-dom";
-
-import router from '@/router.jsx'
-
+import router from '@/router.js'
+// redux
+import { Provider as ReduxProvider } from 'react-redux'
+import store from './store'
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <ReduxProvider store={store}>
+            <RouterProvider router={router} />
+        </ReduxProvider>
     </React.StrictMode>
 );
