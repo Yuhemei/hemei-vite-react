16696dd59966602e9d819948b0ef1e894ff6a3ae
hemei <yuhemei8088@163.com>
2023-03-27 10:09:18 +0800

    feat(page):关于路由相关的页面路径修改完成

--- src/screens/RouterTest/root.jsx
+++ src/screens/RouterTest/root.jsx
@@ -89,7 +89,7 @@ export default function Root() {
                   <li key={contact.id}>
                     <Link to={`router${contact.id}`}></Link>
                     <NavLink
-                      to={`contacts/${contact.id}`}
+                      to={`routerTest/contacts/${contact.id}`}
                       className={({ isActive, isPending }) =>
                         isActive ? "active" : isPending ? "pending" : ""
                       }
